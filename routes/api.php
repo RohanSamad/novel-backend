5c4834a2d2b5f6322d117f0166dd6ccd6585961d
mitprogrammerinfo <mitprogrammerinfo@gmail.com>
2025-05-31 08:37:20 +0500

    set novel apis

--- routes/api.php
+++ routes/api.php
@@ -11,27 +11,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [UserAuthController::class, 'login']);
 Route::post('/register', [UserAuthController::class, 'register']);
+
  Route::prefix('authors')->group(function () {
         Route::get('/', [AuthorController::class, 'index'])->name('authors.index');
         Route::get('/{id}', [AuthorController::class, 'show'])->name('authors.show');
     });
+    
     Route::prefix('chapters')->group(function () {
         Route::get('/novel/{novelId}', [ChapterController::class, 'index'])->name('chapters.index');
         Route::get('/novel/{novelId}/{chapterId}', [ChapterController::class, 'show'])->name('chapters.show');
         Route::get('/recent', [ChapterController::class, 'recent'])->name('chapters.recent');
     });
+
+      Route::prefix('novels')->group(function () {
+        Route::get('/', [NovelController::class, 'index'])->name('novels.index');
+        Route::get('/{id}', [NovelController::class, 'show'])->name('novels.show');
+        Route::get('/author/{authorId}', [NovelController::class, 'byAuthor'])->name('novels.byAuthor');
+        Route::get('/genre/{genreSlug}', [NovelController::class, 'byGenre'])->name('novels.byGenre');
+    });
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/novels/search', [NovelController::class, 'search']);
     Route::get('/check-session', [UserAuthController::class, 'checkSession']);
     Route::post('/logout', [UserAuthController::class, 'logout']);
    
-    Route::prefix('novels')->group(function () {
-        Route::get('/', [NovelController::class, 'index'])->name('novels.index');
-        Route::get('/{id}', [NovelController::class, 'show'])->name('novels.show');
-        Route::get('/author/{authorId}', [NovelController::class, 'byAuthor'])->name('novels.byAuthor');
-       // Route::get('/search', [NovelController::class, 'search'])->name('novels.search');
-        Route::get('/genre/{genreSlug}', [NovelController::class, 'byGenre'])->name('novels.byGenre');
-    });
+  
     Route::prefix('featured-novels')->group(function () {
         Route::get('/', [FeaturedNovelController::class, 'index'])->name('featured-novels.index');
     });
