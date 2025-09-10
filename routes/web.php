0d5d84fa08d210b89befb3d7f0cadb6f9feb86f1
RohanSamad <rohansamad@proton.me>
2025-09-10 12:34:52 +0500

    debugging upload limit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Temporary debug route
+Route::get('/phpinfo', function () {
+    phpinfo();
+});
